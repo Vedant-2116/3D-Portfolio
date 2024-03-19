727da3a407e9c1eac55384d06a532bf234876f54
Vedant2116 <vedqntgohel@gmail.com>
2024-03-19 19:20:08 -0400

    Commit

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -356,92 +356,93 @@ const ContactSection = () => {
   };
 
   return (
-<Section>
-  <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
-  <form onSubmit={handleSubmit}>
-    <div className="flex">
-      <div className="mt-8 p-8 rounded-md w-96 max-w-full mr-4">
-        {/* Phone Number */}
-        <div className="flex items-center mb-4">
-          <FaPhoneAlt
-            onClick={handlePhoneClick}
-            className="text-indigo-600 cursor-pointer"
-            size={24}
-          />
-          <p className="ml-4 text-gray-800 text-lg">{phoneNumber}</p>
-        </div>
-      </div>
+    <Section>
+      <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-wrap">
+          <div className="mt-8 p-4 rounded-md w-48 md:w-64 max-w-full mr-4">
+            {/* Phone Number */}
+            <div className="flex items-center">
+              <FaPhoneAlt
+                onClick={handlePhoneClick}
+                className="text-indigo-600 cursor-pointer"
+                size={24}
+              />
+              <p className="ml-2 md:ml-4 text-gray-800 text-lg">{phoneNumber}</p>
+            </div>
+          </div>
 
-      <div className="mt-8 p-8 rounded-md w-96 max-w-full">
-        {/* Address */}
-        <div className="flex items-center">
-          <FaMapMarkerAlt
-            onClick={handleLocationClick}
-            className="text-indigo-600 cursor-pointer"
-            size={24}
-          />
-          <p className="ml-4 text-gray-800 text-lg">{address}</p>
+          <div className="mt-8 p-4 rounded-md w-48 md:w-64 max-w-full">
+            {/* Address */}
+            <div className="flex items-center">
+              <FaMapMarkerAlt
+                onClick={handleLocationClick}
+                className="text-indigo-600 cursor-pointer"
+                size={24}
+              />
+              <p className="ml-2 md:ml-4 text-gray-800 text-lg">{address}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
-      {state.succeeded ? (
-        <p className="text-gray-900 text-center">Thanks for your message!</p>
-      ) : (
-        <>
-          <label htmlFor="name" className="font-medium text-gray-900 block mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <label
-            htmlFor="email"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <ValidationError
-            className="mt-1 text-red-500"
-            prefix="Email"
-            field="email"
-            errors={state.errors}
-          />
-          <label
-            htmlFor="message"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <ValidationError
-            className="mt-1 text-red-500"
-            errors={state.errors}
-          />
-          <button
-            disabled={state.submitting}
-            className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
-          >
-            Submit
-          </button>
-        </>
-      )}
-    </div>
-  </form>
-</Section>
+        <div className="mt-8 p-4 rounded-md bg-white bg-opacity-50 w-96 md:w-full">
+          {state.succeeded ? (
+            <p className="text-gray-900 text-center">Thanks for your message!</p>
+          ) : (
+            <>
+              <label htmlFor="name" className="font-medium text-gray-900 block mb-1">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-2 md:p-3"
+              />
+              <label
+                htmlFor="email"
+                className="font-medium text-gray-900 block mb-1 mt-8"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-2 md:p-3"
+              />
+              <ValidationError
+                className="mt-1 text-red-500"
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+              <label
+                htmlFor="message"
+                className="font-medium text-gray-900 block mb-1 mt-8"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="h-24 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-2 md:p-3"
+              />
+              <ValidationError
+                className="mt-1 text-red-500"
+                errors={state.errors}
+              />
+              <button
+                disabled={state.submitting}
+                className="bg-indigo-600 text-white py-3 px-6 rounded-lg font-bold text-lg mt-8"
+              >
+                Submit
+              </button>
+            </>
+          )}
+        </div>
+      </form>
+    </Section>
   );
 };
+
