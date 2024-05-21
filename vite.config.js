8812e7d4b5c5dcabdab8ef2bf172776fedb6b3d8
Vedant2116 <vedqntgohel@gmail.com>
2024-05-20 23:08:45 -0400

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3D-Portfolio/', // Make sure this matches your repo name
   plugins: [react()],
-})
+});
